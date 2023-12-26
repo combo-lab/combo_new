c794d6f669d7cd84ef321757244dfe47ca0ea865
c4710n <c4710n@users.noreply.github.com>
2023-12-26 14:18:49 +0800

    init: mix new needle_new

---
+++ lib/needle_new.ex
@@ -0,0 +1,18 @@
+defmodule NeedleNew do
+  @moduledoc """
+  Documentation for `NeedleNew`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NeedleNew.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/needle_new_test.exs
@@ -0,0 +1,8 @@
+defmodule NeedleNewTest do
+  use ExUnit.Case
+  doctest NeedleNew
+
+  test "greets the world" do
+    assert NeedleNew.hello() == :world
+  end
+end
