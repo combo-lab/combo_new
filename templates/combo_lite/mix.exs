26077d87af43ae37e615031f0d8c6ab7a37772f2
c4710n <c4710n@users.noreply.github.com>
2024-01-06 20:35:40 +0800

    feat(template/combo_lite): update misc files

--- templates/combo_lite/mix.exs
+++ templates/combo_lite/mix.exs
@@ -1,15 +1,19 @@
 defmodule ComboLite.MixProject do
   use Mix.Project
 
+  @app :combo_lite
+  @version "0.1.0"
+
   def project do
     [
-      app: :combo_lite,
-      version: "0.1.0",
+      app: @app,
+      version: @version,
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
-      deps: deps()
+      releases: releases(),
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -27,48 +31,100 @@ defmodule ComboLite.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  # Specifies configuration for releases.
+  #
+  # Type `mix help release` for examples and options.
+  defp releases do
+    [
+      {@app,
+       [
+         include_executables_for: [:unix]
+       ]}
+    ]
+  end
+
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.10"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.1"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.2"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"},
+      {:dns_cluster, "~> 0.1.1"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:cozy_proxy, "~> 0.3"},
+      {:plug_cowboy, "~> 2.5"},
+      {:plug_probe, "~> 0.1"},
+
+      # i18n
+      {:ex_cldr, "~> 2.37"},
+      {:gettext, "~> 0.20"},
+
+      # web
+      {:phoenix, "~> 1.7.10"},
+      {:phoenix_html, "~> 4.0"},
+      {:phoenix_live_view, "~> 0.20.2"},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:floki, ">= 0.30.0", only: :test}
     ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      setup: ["deps.get", "assets.setup", "assets.build"],
+
+      # for testing release locally
+      "release.local": [
+        "assets.setup",
+        "assets.deploy",
+        "release",
+        "assets.clean"
+      ],
+
+      # ! summary
+
+      "assets.setup": [
+        "user_web.assets.setup"
+      ],
+      "assets.build": [
+        "user_web.assets.build"
+      ],
+      "assets.deploy": [
+        "user_web.assets.deploy"
+      ],
+      "assets.clean": [
+        "user_web.assets.clean"
+      ],
+
+      #! i18n
+
+      "i18n.gettext.merge": [
+        "gettext.extract",
+        "gettext.merge priv/i18n/gettext"
+      ],
+      "i18n.gettext.add_locale": [
+        "gettext.merge priv/i18n/gettext --locale"
+      ],
+
+      # ! user_web
+
+      "user_web.assets.setup": [
+        "cmd npm install --prefix assets/user_web"
+      ],
+      "user_web.assets.build": [
+        "cmd npm run build --prefix assets/user_web"
+      ],
+      "user_web.assets.deploy": [
+        "cmd npm run build --prefix assets/user_web",
+        "cmd mix phx.digest priv/user_web/static"
+      ],
+      "user_web.assets.clean": [
+        "cmd mix phx.digest.clean --all -o priv/user_web/static"
+      ]
     ]
   end
 end
