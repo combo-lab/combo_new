4adfbad82ce615e5197def61bbc0d29d28e44be1
c4710n <c4710n@users.noreply.github.com>
2024-01-06 20:06:58 +0800

    feat: init this project as mix project

---
+++ lib/combo_new.ex
@@ -0,0 +1,18 @@
+defmodule ComboNew do
+  @moduledoc """
+  Documentation for `ComboNew`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ComboNew.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/combo_new_test.exs
@@ -0,0 +1,8 @@
+defmodule ComboNewTest do
+  use ExUnit.Case
+  doctest ComboNew
+
+  test "greets the world" do
+    assert ComboNew.hello() == :world
+  end
+end
