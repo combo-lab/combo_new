74e64dcc4a1ffc3c681bd60f2f5a1836b7cd2410
c4710n <c4710n@users.noreply.github.com>
2024-01-07 12:36:00 +0800

    refactor: move templates out of priv

--- lib/combo_new/generator/combo_lite.ex
+++ lib/combo_new/generator/combo_lite.ex
@@ -2,7 +2,7 @@ defmodule ComboNew.Generator.ComboLite do
   @moduledoc false
 
   use ComboNew.Generator,
-    template_path: Path.expand("../../../priv/templates/combo_lite", __DIR__),
+    template_path: Path.expand("../../../templates/combo_lite", __DIR__),
     template_app: :combo_lite,
     template_module: ComboLite,
     template_env_prefix: "COMBO_LITE"
--- lib/combo_new/generator/combo_saas.ex
+++ lib/combo_new/generator/combo_saas.ex
@@ -2,7 +2,7 @@ defmodule ComboNew.Generator.ComboSaaS do
   @moduledoc false
 
   use ComboNew.Generator,
-    template_path: Path.expand("../../../priv/templates/combo_saas", __DIR__),
+    template_path: Path.expand("../../../templates/combo_saas", __DIR__),
     template_app: :combo_saas,
     template_module: ComboSaaS,
     template_env_prefix: "COMBO_SAAS"
