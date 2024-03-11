616e863ef5e53e8ec1aec99769d5d031f83dcebc
c4710n <c4710n@users.noreply.github.com>
2024-03-11 09:22:56 +0800

    feat(templates): remove view from doc

--- templates/combo_saas/lib/admin_web.ex
+++ templates/combo_saas/lib/admin_web.ex
@@ -124,7 +124,7 @@ defmodule ComboSaaS.AdminWeb do
   end
 
   @doc """
-  When used, dispatch to the appropriate controller/view/etc.
+  When used, dispatch to the appropriate controller, live_view, etc.
   """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
