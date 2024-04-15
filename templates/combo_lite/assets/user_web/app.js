5a885aebe411a6f262d7318336ce290010a854f7
c4710n <c4710n@users.noreply.github.com>
2024-04-15 23:11:13 +0800

    feat(templates): improve app.js

--- templates/combo_lite/assets/user_web/app.js
+++ templates/combo_lite/assets/user_web/app.js
@@ -1,6 +1,6 @@
 import '@fontsource-variable/inter'
 import '@fontsource-variable/jetbrains-mono'
 
-import './lib/phoenix'
 import './lib/tailwind'
+import './lib/phoenix'
 import './lib/phoenix_live_view'
