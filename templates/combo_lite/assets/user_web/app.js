c995e650ba18a2d5533f7e3d0a60f07598e9d2b1
c4710n <c4710n@users.noreply.github.com>
2024-04-26 15:11:33 +0800

    feat(templates): add lib/css-default to assets

--- templates/combo_lite/assets/user_web/app.js
+++ templates/combo_lite/assets/user_web/app.js
@@ -1,6 +1,8 @@
 import '@fontsource-variable/inter'
 import '@fontsource-variable/jetbrains-mono'
 
+import './lib/css-default'
+
 import './lib/tailwind'
 import './lib/alpine'
 import './lib/phoenix'
