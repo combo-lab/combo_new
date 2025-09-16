c105eaffad4bacc16f31ace919df39f096948d6e
Zeke Dou <zeke@zekedou.live>
2025-09-16 20:39:45 +0800

    feat(templates/vite): remove TailwindCSS, because I don't want to make assumptions about users' preferences
    If they like TailwindCSS, they can integrate it themselves.

--- templates/vite/assets/vite.config.js
+++ templates/vite/assets/vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite"
 import combo from "vite-plugin-combo"
-import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
     combo({
       input: ["src/css/app.css", "src/js/app.js"],
       staticDir: "../priv/static",
-      refresh: [
-        "../lib/*/web/router.ex",
-        "../lib/*/web/(controllers|layouts|components)/**/*.(ex|ceex)",
-      ],
     }),
-    tailwindcss()
   ],
 })
