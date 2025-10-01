140e9eaf06d1e9a065dd75b2b978cd6acf9966e6
Zeke Dou <zeke@zekedou.live>
2025-10-01 22:47:32 +0800

    docs(inertia-react): add doc for inertia-helper.js

--- templates/inertia-react/assets/src/js/inertia-helper.js
+++ templates/inertia-react/assets/src/js/inertia-helper.js
@@ -1,3 +1,24 @@
+// An Inertia helper for resolving page component.
+//
+// # Usage
+//
+// ## Resolve page component
+// 
+//     resolvePageComponent(
+//       `./Pages/${name}.jsx`,
+//       import.meta.glob('./Pages/**/*.jsx', { eager: true })
+//     )
+//
+// ## Resolve page component with a fallback name
+// 
+//     resolvePageComponent(
+//       `./Pages/${name}.jsx`,
+//       import.meta.glob('./Pages/**/*.jsx', { eager: true }),
+//       {
+//         fallbackName: `./Pages/404.jsx`
+//       }
+//     )
+//
 export async function resolvePageComponent(name, pages, options) {
   if (typeof pages[name] === 'undefined' && options?.fallbackName) {
     name = options.fallbackName
